b2bc8e764da59096f3f03c240126ad4a4dfedb8e
samadhannnn <samadhanbodkhe222@gmail.com>
2025-11-13 21:19:20 +0530

    Add files via upload

---
+++ scripts/deploy.js
@@ -0,0 +1,69 @@
+// We require the Hardhat Runtime Environment explicitly here. This is optional
+// but useful for running the script in a standalone fashion through `node <script>`.
+//
+// You can also run a script with `npx hardhat run <script>`. If you do that, Hardhat
+// will compile your contracts, add the Hardhat Runtime Environment's members to the
+// global scope, and execute the script.
+const hre = require("hardhat");
+
+const tokens = (n) => {
+  return ethers.utils.parseUnits(n.toString(), "ether");
+};   
+
+async function main() {
+
+  //SetUp accounts
+      [buyer, seller, inspector, lender] = await ethers.getSigners();
+
+// Deploying the real Estate
+      const RealEstate = await ethers.getContractFactory("RealEstate");
+     const realEstate = await RealEstate.deploy();
+     await realEstate.deployed();
+
+     
+    console.log(`Minting 3 properties....\n`) 
+
+      for(let i=0 ; i<3;i++)
+      {
+        const transaction= await realEstate.connect(seller).mint(`https://ipfs.io/ipfs/QmQUozrHLAusXDxrvsESJ3PYB3rUeUuBAvVWw6nop2uu7c/1/${i+1}`)
+      }
+      //deployed Escrow
+      const Escrow= await ethers.getContractFactory('Escrow')
+      const escrow = await Escrow.deploy(
+        realEstate.address,
+        seller.address,
+        inspector.address,
+        lender.address
+      )
+      await escrow.deployed()
+
+      console.log(`Deployed Escrow Contract at: ${escrow.address}`) 
+
+      for(let i=0; i<3;i++)
+      {
+        //Approves properites...
+        let transaction = await realEstate.connect(seller).approve(escrow.address,i+1)
+        await transaction.wait();
+
+      }
+
+      //Listing  properties
+      transaction = await escrow.connect(seller).list(1,buyer.address,tokens(20),tokens(10))
+      await transaction.wait()
+
+      transaction = await escrow.connect(seller).list(2,buyer.address,tokens(15),tokens(5))
+      await transaction.wait()
+
+      transaction = await escrow.connect(seller).list(3,buyer.address,tokens(10),tokens(5))
+      await transaction.wait()
+
+      console.log("Finished ..." )
+
+    }
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
